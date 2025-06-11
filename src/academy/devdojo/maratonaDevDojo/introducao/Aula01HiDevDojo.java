8e644507530379d350fe683c40672d26c615696b
Vinicius Correa dos Santos <79265270+Vinicius-CSantos@users.noreply.github.com>
2025-06-10 22:02:21 -0300

    AulasIniciais

---
+++ src/academy/devdojo/maratonaDevDojo/introducao/Aula01HiDevDojo.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonaDevDojo.introducao;
+
+public class Aula01HiDevDojo {
+    public static void main(String[] args){
+        System.out.println("Hi");
+    }
+}
---
+++ src/academy/devdojo/maratonaDevDojo/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonaDevDojo.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main (String [] args){
+    //Tipos Primitivos.
+        int age = 10;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 87;
+
+        System.out.println(
+                "A idade dele é: " + age+ " anos"
+        );
+        System.out.println(falso);
+        System.out.println(caractere);
+    }
+}
